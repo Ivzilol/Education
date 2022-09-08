9eaeca77186fa194ceee5524f958f163ea77aa9c
Ivaylo Alichkov <ivo_ali@abv.bg>
2022-09-08 16:38:34 +0300

    Education

---
+++ Advanced/Sets And Maps Advanced/Unleashed_12.java
@@ -0,0 +1,11 @@
+package Exercises_03;
+
+import java.util.Scanner;
+
+public class Unleashed_12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
