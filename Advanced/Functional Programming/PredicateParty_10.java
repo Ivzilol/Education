a2667d7288dac1dca009e8fb88567421fe49fd1f
Ivaylo Alichkov <ivo_ali@abv.bg>
2022-09-25 10:00:17 +0300

    Education

---
+++ Advanced/Functional Programming/PredicateParty_10.java
@@ -0,0 +1,11 @@
+package Exercises_05;
+
+import java.util.Scanner;
+
+public class PredicateParty_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
