269e53746969bc3f2411d40e5f9dc1bb3722325b
Ivaylo Alichkov <ivo_ali@abv.bg>
2022-09-26 13:04:04 +0300

    Education

---
+++ Advanced/Functional Programming/ThePartyReservationFilterModule_11.java
@@ -0,0 +1,11 @@
+package Exercises_05;
+
+import java.util.Scanner;
+
+public class ThePartyReservationFilterModule_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
