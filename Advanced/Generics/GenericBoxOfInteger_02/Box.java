559252a28dc0e908aa79ce1086e1ada2b89b6c6b
Ivaylo Alichkov <ivo_ali@abv.bg>
2022-10-05 20:09:27 +0300

    Education

---
+++ Advanced/Generics/GenericBoxOfInteger_02/Box.java
@@ -0,0 +1,25 @@
+package Exercises_07.GenericBoxOfInteger_02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> {
+    private List<T> elements;
+
+    public Box() {
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        this.elements.add(element);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (T element : elements) {
+            builder.append(element.getClass().getName() + ": " + element).append("\n");
+        }
+        return builder.toString();
+    }
+}
