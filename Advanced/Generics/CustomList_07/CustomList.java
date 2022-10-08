fa576e2db48235d50654cba5cddfb93c541681bd
Ivaylo Alichkov <ivo_ali@abv.bg>
2022-10-08 19:23:25 +0300

    Education

---
+++ Advanced/Generics/CustomList_07/CustomList.java
@@ -0,0 +1,65 @@
+package Exercises_07.CustomList_07;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class CustomList<T extends Comparable<T>> {
+
+    private List<T> elements;
+
+    public CustomList() {
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        this.elements.add(element);
+    }
+
+    public T remove(int index) {
+        if (index >= 0 && index <= this.elements.size() - 1) {
+            return this.elements.remove(index);
+        }
+        throw new IndexOutOfBoundsException("Invalid Index");
+    }
+
+    public boolean contains(T element) {
+        return this.elements.contains(element);
+    }
+
+    public void swap(int firstIndex, int secondIndex) {
+        T firstIndexElement = this.elements.get(firstIndex);
+        T secondIndexElement = this.elements.get(secondIndex);
+        this.elements.set(firstIndex, secondIndexElement);
+        this.elements.set(secondIndex, firstIndexElement);
+    }
+
+    public int countGreaterThan(T element) {
+        int count = 0;
+        for (T elementInList : this.elements) {
+            if (elementInList.compareTo(element) > 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public T getMax() {
+        return this.elements.stream()
+                .max(Comparator.naturalOrder())
+                .get();
+    }
+
+    public T getMin() {
+        return this.elements.stream()
+                .min((e1, e2) -> e1.compareTo(e2))
+                .get();
+    }
+
+    public void print() {
+        for (T element : this.elements) {
+            System.out.println(element);
+        }
+    }
+
+}
---
+++ Advanced/Generics/CustomList_07/Main.java
@@ -0,0 +1,49 @@
+package Exercises_07.CustomList_07;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CustomList<String> customList = new CustomList<>();
+
+        String command = scanner.nextLine();
+        while (!command.equals("END")) {
+            String commandType = command.split("\\s+")[0];
+            switch (commandType) {
+                case "Add":
+                    String element = command.split("\\s+")[1];
+                    customList.add(element);
+                    break;
+                case "Remove":
+                    int indexToRemove = Integer.parseInt(command.split("\\s+")[1]);
+                    customList.remove(indexToRemove);
+                    break;
+                case "Contains":
+                    String searchedElement = command.split("\\s+")[1];
+                    System.out.println(customList.contains(searchedElement));
+                    break;
+                case "Swap":
+                    int firstIndex = Integer.parseInt(command.split("\\s+")[1]);
+                    int secondIndex = Integer.parseInt(command.split("\\s+")[2]);
+                    customList.swap(firstIndex, secondIndex);
+                    break;
+                case "Greater":
+                    String elementForCheck = command.split("\\s+")[1];
+                    System.out.println(customList.countGreaterThan(elementForCheck));
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Print":
+                    customList.print();
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+    }
+}
