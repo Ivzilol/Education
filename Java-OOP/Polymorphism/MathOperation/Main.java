bcae15cd09e8dc2a60ac69287f9a4ab464143ad3
Ivaylo Alichkov <ivo_ali@abv.bg>
2022-10-19 10:59:18 +0300

    Java-OOP

---
+++ Java-OOP/Polymorphism/MathOperation/Main.java
@@ -0,0 +1,9 @@
+package Lab_05.MathOperation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MathOperation.add(1, 2);
+
+    }
+}
---
+++ Java-OOP/Polymorphism/MathOperation/MathOperation.java
@@ -0,0 +1,15 @@
+package Lab_05.MathOperation;
+
+public class MathOperation {
+
+    public static int add(int num1, int num2){
+        return num1 + num2;
+    }
+    public static int add(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+    public static int add(int num1, int num2, int num3, int num4){
+        return num1 + num2 + num3 + num4;
+    }
+
+}
