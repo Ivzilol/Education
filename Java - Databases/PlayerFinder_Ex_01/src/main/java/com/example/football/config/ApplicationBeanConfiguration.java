08b67ec60e66f4716a5030b3e6d1d99d0ec03b61
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-06-30 15:35:40 +0300

    Remove

--- Java - Databases/PlayerFinder_Ex_01/src/main/java/com/example/football/config/ApplicationBeanConfiguration.java
+++
@@ -1,5 +0,0 @@
-package com.example.football.config;
-
-//ToDo Create configurations
-public class ApplicationBeanConfiguration {
-}
--- Java - Databases/PlayerFinder_Ex_01/src/main/java/com/example/football/models/entity/PlayerPosition.java
+++
@@ -1,6 +0,0 @@
-package com.example.football.models.entity;
-
-public enum PlayerPosition {
-    ATT, MID, DEF
-
-}
