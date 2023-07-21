ef83b544cdf625f6b403f52250443c57706f38bd
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-07-21 14:05:09 +0300

    ExamPrep

---
+++ Java - Databases/Exams/Forecast/skeleton/src/main/java/softuni/exam/models/dto/CountrySeedDto.java
@@ -0,0 +1,33 @@
+package softuni.exam.models.dto;
+
+import com.google.gson.annotations.Expose;
+
+import javax.validation.constraints.Size;
+
+public class CountrySeedDto {
+
+    @Expose
+    @Size(min = 2, max = 60)
+    private String countryName;
+
+    @Expose
+    @Size(min = 2, max = 20)
+    private String currency;
+
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
