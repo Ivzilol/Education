97c17ab14f799cc854288307f0da560c80c932d9
Ivaylo Alichkov <ivo_ali@abv.bg>
2023-10-21 09:51:04 +0300

    Refactor

--- Java - Web/Exams/ShoppingList/src/main/java/com/example/ShoppingList/model/dto/HouseholdsDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.ShoppingList.model.dto;
-
-import java.math.BigDecimal;
-
-public class HouseholdsDTO extends FoodDTO{
-    public HouseholdsDTO(Long id, String name, BigDecimal price) {
-        super(id, name, price);
-    }
-}
--- Java - Web/Exams/ShoppingList/src/main/java/com/example/ShoppingList/model/dto/OthersDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.ShoppingList.model.dto;
-
-import java.math.BigDecimal;
-
-public class OthersDTO extends FoodDTO{
-    public OthersDTO(Long id, String name, BigDecimal price) {
-        super(id, name, price);
-    }
-}
